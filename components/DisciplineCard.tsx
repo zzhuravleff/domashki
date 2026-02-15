158a2625011afd3af7879410e6613f15aed8c182
zzhuravleff <aqua197604@gmail.com>
2026-02-15 22:02:42 +0300

    add days

--- components/DisciplineCard.tsx
+++ components/DisciplineCard.tsx
@@ -1,7 +1,10 @@
 "use client";
 
 import { Button } from "@heroui/button";
+import {Chip} from "@heroui/chip";
 import { Discipline } from "@/types";
+
+const daysLabels = ["Пн","Вт","Ср","Чт","Пт","Сб","Вс"];
 
 type Props = {
   discipline: Discipline;
@@ -10,7 +13,7 @@ type Props = {
 };
 
 export default function DisciplineCard({ discipline, onDone, onEdit }: Props) {
-  const { name, task, isLongTerm } = discipline;
+  const { name, task, isLongTerm, days } = discipline;
 
   let color = "text-green-500";
 
@@ -19,13 +22,29 @@ export default function DisciplineCard({ discipline, onDone, onEdit }: Props) {
 
   return (
     <div className="bg-white/70 shadow-none rounded-3xl p-3 flex gap-2 justify-between">
-      <div className="flex flex-col">
-          <h3 className={`font-semibold text-lg line-clamp-2 leading-5 ${color}`}>{name}</h3>
-          {task && (
-            <p className="text-base text-gray-600 mt-1 whitespace-pre-wrap">
-              {task}
-            </p>
-          )}
+      <div className="flex flex-col justify-between">
+          <div className="flex flex-col">
+            <h3 className={`font-semibold text-lg line-clamp-2 leading-5 ${color}`}>{name}</h3>
+            {task && (
+              <p className="text-base text-gray-600 mt-1 whitespace-pre-wrap">
+                {task}
+              </p>
+            )}
+          </div>
+          {/* 🔹 Отображаем дни недели */}
+        {days && days.length > 0 && (
+          <div className="flex flex-wrap gap-1 mt-2">
+            {days.map((d) => (
+              <Chip
+                key={d}
+                radius="full"
+                variant="faded"
+              >
+                {daysLabels[d - 1]} {/* d: 1..7 */}
+              </Chip>
+            ))}
+          </div>
+        )}
         </div>
         <div className="flex flex-col gap-2">
           {task && (
--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "domashki-static-v1.0.1";
+const CACHE = "domashki-static-v1.0.10";
 const STATIC_ASSETS = [
   "/manifest.json",
   "/icons/icon-192.png",
